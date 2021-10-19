3affe9bef48733b9c7f70dba0329d144a0016678
Sandra Rosa Antony <84260242+Sandra-Rosa@users.noreply.github.com>
2021-10-19 11:43:10 +0530

    uploaded data types file

---
+++ datatypes.py
@@ -0,0 +1,64 @@
+#Getting the data type
+x = 5
+print(type(x))
+
+#setting the data type
+#str 
+x = "Hello world"
+#int
+x = 20
+#float
+x = 20.5
+#complex
+x = 1j
+#list
+x = ["apple","banana","cherry"]
+#tuple
+x = ("apple","banana","cherry")
+#range
+x = range(6)
+#dict
+x = {"name" : "John", "age" : 36}
+#set
+x = {"apple","banana","cherry"}
+#frozenset
+x = frozenset({"apple","banana","cherry"})
+#bool
+x = True
+#bytes
+x = b"Hello"
+#bytearray
+x = bytearray(5)
+#memoryview
+x = memoryview(bytes(5))
+
+#setting the specefic data type
+
+#str
+x = str("Hello world")
+#int
+x = int(20)
+#float
+x = float(20.5)
+#complex
+x = complex(1j)
+#list
+x = list(("apple","banana","cherry"))
+#tuple
+x = tuple(("apple","banana","cherry"))
+#range
+x = range(6)
+#dict
+x = dict(name="john", age=36)
+#set
+x = set(("apple","banana","cherry"))
+#frozenset
+x = frozenset(("apple","banana","cherry"))
+#bool
+x = bool(5)
+#bytes
+x = bytes(5)
+#bytearray
+x = bytearray(5)
+#memoryview
+x = memoryview(bytes(5))
