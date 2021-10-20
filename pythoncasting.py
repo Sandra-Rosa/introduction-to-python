95c0ecf0ac39e2b0ec82e6a7f6ec954a1e0a57f1
Sandra Rosa Antony <84260242+Sandra-Rosa@users.noreply.github.com>
2021-10-20 09:27:42 +0530

    added python casting file

---
+++ pythoncasting.py
@@ -0,0 +1,14 @@
+ x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+#floats
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = ("4.2")
+
+#strings
+x = str("s1")
+y = str(2)
+z = str(3.0)
