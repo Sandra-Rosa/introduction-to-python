dfbb03befe13dccf70cebee676003b82b713ac29
Sandra Rosa Antony <84260242+Sandra-Rosa@users.noreply.github.com>
2021-10-21 09:38:32 +0530

    Add files via upload

---
+++ pythonoperators.py
@@ -0,0 +1,102 @@
+#python arithmetic operators
+#sample codes
+x = 1
+y = 2
+#addition
+print(x+y)
+#substraction
+print(x-y)
+#Multiplication
+print(x*y)
+#division
+(x/y)
+#modulus
+print(x % y)
+#exponentiation
+print(x**y)
+#floor division
+print(x//y)
+
+##python assignment operator
+#sample codes
+#=
+x = 5
+print(x)
+#+=
+x +=3
+print(x)
+#-=
+x -= 3
+print(x)
+
+#*=
+x *= 3
+print(x)
+
+#/=
+x /= 3
+print(x)
+#%=
+x %= 3
+print(x)
+#//=
+x //= 3
+print(x)
+#**=
+x **= 3
+print(x)
+#&=
+x &= 3
+print(x)
+#|=
+x |= 3
+print(x)
+#^=
+x ^= 3
+print(x)
+#>>=
+x >>= 3
+print(x)
+#<<=
+x <<= 3
+print(x)
+
+#python comparison operators
+a = 2
+b = 5
+#equal
+print(a == b)
+#not equal
+print(a != b)
+#greater than
+print(a>b)
+#less than
+print(a<b)
+#greater than or equal to
+print(a>=b)
+#less tha or equal to
+print(a >= b)
+
+#python logical operators
+c = 5
+#and
+print(c<5 and c<10)
+#or
+print(c<5 and c<4)
+#not
+print(not(c<5 and c<10))
+
+#python identity operators
+#is
+p = ["apple", "banana"]
+q = ["apple", "banana"]
+r = p
+print(r is p)
+#is not
+print(r is not p)
+
+#python membership operators
+#in
+print("banana" in p)
+#not in
+print("pineapple" not in p)
